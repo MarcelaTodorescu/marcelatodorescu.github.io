ec5e7786895f29f32062883e81b54bedc1f4a998
Marcela Todorescu <todorescumarcela@gmail.com>
2018-11-05 10:43:43 +0200

    update

--- html5/orientation.js
+++ html5/orientation.js
@@ -27,7 +27,15 @@ function on_device_orientation(e)
 
 function on_device_motion(e)
 {
-	document.getElementById("id_acc_z").innerHTML = Math.round(e.accelerationIncludingGravity.z * 100) / 100;
-	document.getElementById("id_acc_x").innerHTML = Math.round(e.accelerationIncludingGravity.x * 100) / 100 ;
-	document.getElementById("id_acc_y").innerHTML = Math.round(e.accelerationIncludingGravity.y * 100) / 100;
+	var acc_z = e.accelerationIncludingGravity.z;
+	var acc_x = e.accelerationIncludingGravity.x;
+	var acc_y = e.accelerationIncludingGravity.y;
+	document.getElementById("id_acc_z").innerHTML = Math.round(acc_z * 100) / 100;
+	document.getElementById("id_acc_x").innerHTML = Math.round(acc_x * 100) / 100;
+	document.getElementById("id_acc_y").innerHTML = Math.round(acc_y * 100) / 100;
+	
+	document.getElementById("id_rot_x").innerHTML = Math.atan(acc_x / acc_z);
+	document.getElementById("id_rot_y").innerHTML = Math.atan(acc_y / acc_z);
+	
+	
 }
