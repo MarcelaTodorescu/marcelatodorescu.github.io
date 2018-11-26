4cb16964de8a3c00b8d3ac4553683ecbd3326332
Marcela Todorescu <todorescumarcela@gmail.com>
2018-11-26 11:39:44 +0200

    linie

--- html5/touch.js
+++ html5/touch.js
@@ -1,10 +1,12 @@
-document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.3";
+document.getElementById("id_bussiness_version").innerHTML = "Bussiness version: 2018.11.26.4";
 
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch_start);
 canvas.addEventListener("touchmove", on_touch_move);
 
 var canvas_bounding_rect = canvas.getBoundingClientRect();
+var last_pos = {x: 0, y: 0);
+
 
 function on_touch_start(e)
 {
@@ -14,6 +16,8 @@ function on_touch_start(e)
 		context.beginPath();
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top, 10, 0, 2*Math.PI);
 		context.stroke();
+		last_pos.x = e.changedTouches.pageX;
+		last_pos.y = e.changedTouches.pageY;
 	}
 }
 
@@ -23,6 +27,9 @@ function on_touch_move(e)
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
+		context.moveTo(last_pos.x - canvas_bounding_rect.left, last_pos.y - canvas_bounding_rect.top);
+		context.lineTo(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top);
+		
 		context.arc(e.changedTouches[i].pageX - canvas_bounding_rect.left, e.changedTouches[i].pageY - canvas_bounding_rect.top, 10, 0, 2*Math.PI);
 		context.stroke();
 	}
