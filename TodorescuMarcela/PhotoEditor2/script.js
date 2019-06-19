beba88fd19d5a42218be42f8600f33853b6de0a2
Marcela Todorescu <todorescumarcela@gmail.com>
2019-06-19 19:09:11 +0300

    upload photo

---
+++ TodorescuMarcela/PhotoEditor2/script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');
+          img.src = URL.createObjectURL(this.files[0]); // set src to file url
+      }
+  });
+});
