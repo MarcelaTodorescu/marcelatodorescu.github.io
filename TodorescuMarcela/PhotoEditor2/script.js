abd0fbb9cb53486b1e8e3f3163606532de0b5122
Marcela Todorescu <todorescumarcela@gmail.com>
2019-06-19 19:13:49 +0300

    update

--- TodorescuMarcela/PhotoEditor2/script.js
+++ TodorescuMarcela/PhotoEditor2/script.js
@@ -2,7 +2,6 @@ window.addEventListener('load', function() {
   document.querySelector('input[type="file"]').addEventListener('change', function() {
       if (this.files && this.files[0]) {
           var img = document.querySelector('img');
-          img.src = URL.createObjectURL(this.files[0]); // set src to file url
-      }
+          img.src = URL.createObjectURL(this.files[0]);
   });
 });
